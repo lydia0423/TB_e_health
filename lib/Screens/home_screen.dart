044c9caaf513a4cd37e0d9601e544cbd8a80fe66
WAI XIONG CHEE <wai.xiong@setel.my>
2021-08-14 13:31:11 +0800

    home screen amd asset generation

---
+++ lib/Screens/home_screen.dart
@@ -0,0 +1,143 @@
+import 'package:tb_e_health/gen/assets.gen.dart';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  static String id = 'HomeScreen';
+
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  Widget _uploadWidget() =>
+      SizedBox(
+        height: 300,
+        width: 300,
+        child: GestureDetector(
+          onTap: () {}, // TODO: 
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+            child: Center(
+              child: Assets.images.iconCloud.svg(),
+            ),
+          ),
+        ),
+      );
+  
+  Widget _featureButton({
+    required Widget child,
+    required void Function()? onTap,
+    required String title,
+  }) => 
+      SizedBox(
+        width: 80,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            GestureDetector(
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(32.0),
+                ),
+                child: SizedBox(
+                  height: 64, width: 64,
+                  child: Center(
+                    child: child,
+                  ),
+                ),
+              ), 
+              onTap: onTap,
+            ),
+            const SizedBox(height: 4),
+            Text(
+              title,
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      );
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: HelloAppBar(),
+      // extendBodyBehindAppBar: true,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 120,
+            child: Center(
+              child: Text(
+                'Hello!',
+                style: Theme.of(context).textTheme.headline4!.copyWith(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12),
+            child: Text(
+              'How can we help you today?',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ),
+          SizedBox(
+            height: 36,
+          ),
+          _uploadWidget(),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Text(
+              'Upload Video',
+                style: Theme.of(context).textTheme.headline6,
+            ),
+          ),
+          const SizedBox(
+            height: 12,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 12,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _featureButton(
+                  child: Assets.images.iconCart.svg(height: 30), 
+                  onTap: () {},
+                  title: 'Drug Delivery',
+                ),
+                _featureButton(
+                  child: Assets.images.iconNote.svg(height: 30), 
+                  onTap: () {},
+                  title: 'Side Effects',
+                ),
+                _featureButton(
+                  child: Assets.images.iconMobileInfo.svg(height: 30), 
+                  onTap: () {},
+                  title: 'TB Info',
+                ),
+                _featureButton(
+                  child: Assets.images.iconSupport.svg(height: 30), 
+                  onTap: () {},
+                  title: 'Tele Consultation',
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+      // bottomNavigationBar: ,
+    );
+  }
+}
---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,125 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter/services.dart';
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  SvgGenImage get iconCart => const SvgGenImage('assets/images/icon_cart.svg');
+  SvgGenImage get iconCloud =>
+      const SvgGenImage('assets/images/icon_cloud.svg');
+  SvgGenImage get iconFaq => const SvgGenImage('assets/images/icon_faq.svg');
+  SvgGenImage get iconHome => const SvgGenImage('assets/images/icon_home.svg');
+  SvgGenImage get iconMobileInfo =>
+      const SvgGenImage('assets/images/icon_mobile_info.svg');
+  SvgGenImage get iconNote => const SvgGenImage('assets/images/icon_note.svg');
+  SvgGenImage get iconPeople =>
+      const SvgGenImage('assets/images/icon_people.svg');
+  SvgGenImage get iconSupport =>
+      const SvgGenImage('assets/images/icon_support.svg');
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+}
+
+class AssetGenImage extends AssetImage {
+  const AssetGenImage(String assetName) : super(assetName);
+
+  Image image({
+    Key? key,
+    ImageFrameBuilder? frameBuilder,
+    ImageLoadingBuilder? loadingBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? width,
+    double? height,
+    Color? color,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = false,
+    bool isAntiAlias = false,
+    FilterQuality filterQuality = FilterQuality.low,
+  }) {
+    return Image(
+      key: key,
+      image: this,
+      frameBuilder: frameBuilder,
+      loadingBuilder: loadingBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      width: width,
+      height: height,
+      color: color,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      filterQuality: filterQuality,
+    );
+  }
+
+  String get path => assetName;
+}
+
+class SvgGenImage {
+  const SvgGenImage(this._assetName);
+
+  final String _assetName;
+
+  SvgPicture svg({
+    Key? key,
+    bool matchTextDirection = false,
+    AssetBundle? bundle,
+    String? package,
+    double? width,
+    double? height,
+    BoxFit fit = BoxFit.contain,
+    AlignmentGeometry alignment = Alignment.center,
+    bool allowDrawingOutsideViewBox = false,
+    WidgetBuilder? placeholderBuilder,
+    Color? color,
+    BlendMode colorBlendMode = BlendMode.srcIn,
+    String? semanticsLabel,
+    bool excludeFromSemantics = false,
+    Clip clipBehavior = Clip.hardEdge,
+  }) {
+    return SvgPicture.asset(
+      _assetName,
+      key: key,
+      matchTextDirection: matchTextDirection,
+      bundle: bundle,
+      package: package,
+      width: width,
+      height: height,
+      fit: fit,
+      alignment: alignment,
+      allowDrawingOutsideViewBox: allowDrawingOutsideViewBox,
+      placeholderBuilder: placeholderBuilder,
+      color: color,
+      colorBlendMode: colorBlendMode,
+      semanticsLabel: semanticsLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      clipBehavior: clipBehavior,
+    );
+  }
+
+  String get path => _assetName;
+}
