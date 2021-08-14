434151ef5ce220ca9aa3c378eb08771074ddaa70
lydia0423 <55584148+lydia0423@users.noreply.github.com>
2021-08-14 20:42:20 +0800

    Configure firebase Auth and login page

---
+++ lib/Models/user.dart
@@ -0,0 +1,100 @@
+class ActiveUser {
+  //^ Attributes
+  String avatar, email, name, age, gender;
+
+  //^ Constructor
+  ActiveUser(this.avatar, this.email, this.name, this.age, this.gender);
+
+  //? Factory - creates the ActiveUser instance from the JSON (database storage type) passed
+  //? When Factory is used, implementing a constructor doesn't always create a new instance of its class
+  factory ActiveUser.fromJson(Map<dynamic, dynamic> json) =>
+      _activeUserFromJson(json);
+
+  //? Converts the ActiveUser into a map of key/value pairs
+  Map<String, String> toJson() => _activeUserToJson(this);
+}
+
+//? Converts map of values from Firestore into ActiveUser class.
+//! This method is invoked by ActiveUser.fromJson. Don't call this method.
+ActiveUser _activeUserFromJson(Map<dynamic, dynamic> json) {
+  return ActiveUser(
+    json["UserAvatar"] as String,
+    json["UserEmail"] as String,
+    json["UserName"] as String,
+    json["UserAge"] as String,
+    json["UserGender"] as String,
+  );
+}
+
+//? Converts the ActiveUser class into key/value pairs
+Map<String, String> _activeUserToJson(ActiveUser instance) => <String, String>{
+      "UserAvatar": instance.avatar,
+      "UserEmail": instance.email,
+      "UserName": instance.name,
+      "UserAge": instance.age,
+      "UserGender": instance.gender,
+    };
+
+// //? Retrieves list of all users
+// Future<List<ActiveUser>> getAllUsers() async {
+//   List<ActiveUser> allUser = [];
+//   await FirebaseFirestore.instance
+//       .collection("User")
+//       .get()
+//       .then((value) => value.docs.forEach((element) {
+//             allUser.add(ActiveUser.fromJson(element.data()));
+//           }))
+//       .catchError((onError) => print(onError));
+//   return allUser;
+// }
+
+// //? Retrieve data from Firestore - Use with FutureBuilder
+// Future<DocumentSnapshot> getActiveUser() async {
+//   CollectionReference userDb = FirebaseFirestore.instance.collection("User");
+//   final User currentUser = FirebaseAuth.instance.currentUser;
+//   final currentId = currentUser.uid;
+//   final activeUserDetails = await userDb.doc(currentId).get();
+//   return activeUserDetails;
+// }
+
+// //? Retrieves data from Firestore and stores in an ActiveUser object
+// Future<ActiveUser> myActiveUser({String docId}) async {
+//   String currentId;
+//   User currentUser = FirebaseAuth.instance.currentUser;
+//   (docId == null) ? currentId = currentUser.uid : currentId = docId;
+
+//   var activeUserDetails =
+//       await FirebaseFirestore.instance.collection("User").doc(currentId).get();
+
+//   final ActiveUser activeUser = ActiveUser.fromJson(activeUserDetails.data());
+//   return activeUser;
+// }
+
+// //? Checks if the user email exists in the database
+// Future<String> findUser(String queryField, String queryItem) async {
+//   String docId;
+//   try {
+//     QuerySnapshot snapshot = await FirebaseFirestore.instance
+//         .collection("User")
+//         .where(queryField, isEqualTo: queryItem)
+//         .get();
+//     docId = snapshot.docs[0].id;
+//   } catch (e) {
+//     print("$e : User not found");
+//   }
+//   return docId;
+// }
+
+// //? Saves the role that the user is logged in as
+// Future<void> saveRole(String role) async {
+//   String userId = FirebaseAuth.instance.currentUser.uid;
+//   if (userId != null) {
+//     var loggedInAs = FirebaseFirestore.instance
+//         .collection("User")
+//         .doc(userId)
+//         .collection("Login")
+//         .doc("LoginRole");
+//     await loggedInAs.set({"LoggedInAs": role});
+//   }
+//   print("You are logged in as a $role");
+// }
---
+++ lib/Screens/login.dart
@@ -0,0 +1,204 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:bot_toast/bot_toast.dart';
+import 'package:get/get.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final emailCtrl = TextEditingController();
+  final passwordCtrl = TextEditingController();
+
+  String enteredEmail = "";
+  //^ Password field attributes - password visibility toggle + icon
+  bool passwordHidden = true;
+  var iconShowPassword;
+
+  //? Intialises inital value of variables
+  @override
+  void initState() {
+    enteredEmail = "";
+    passwordHidden = true;
+    iconShowPassword = Icons.visibility_off_rounded;
+    super.initState();
+  }
+
+  //? Build method for Login screen
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                    top: 50.0, bottom: 0.0, left: 40.0, right: 30.0),
+                child: Image(
+                  image: AssetImage('assets/Images/uni_logo.png'),
+                  width: 350,
+                ),
+              ),
+            ),
+            Container(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                    top: 0.0, bottom: 10.0, left: 75.0, right: 60.0),
+                child: Image(
+                  image: AssetImage('assets/Images/app_logo.png'),
+                  width: 280,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 60,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  top: 20.0, bottom: 0.0, left: 30.0, right: 30.0),
+              child: TextField(
+                controller: emailCtrl,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30.0)),
+                  labelText: 'Username',
+                ),
+                onChanged: (newText) {
+                  setState(() {
+                    enteredEmail = newText;
+                  });
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  top: 20.0, bottom: 10.0, left: 30.0, right: 30.0),
+              child: TextField(
+                controller: passwordCtrl,
+                obscureText: passwordHidden,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30.0)),
+                  labelText: 'Password',
+                  suffixIcon: IconButton(
+                    icon: Icon(iconShowPassword),
+                    color: Colors.black,
+                    onPressed: () {
+                      setState(() {
+                        passwordHidden = !passwordHidden;
+                        (passwordHidden)
+                            ? iconShowPassword = Icons.visibility_off_rounded
+                            : iconShowPassword = Icons.visibility_rounded;
+                      });
+                    },
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                GestureDetector(
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 30.0, top: 10),
+                    child: Text('Forget Password?'),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10.0),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 30.0),
+                  child: OutlinedButton(
+                      onPressed: null,
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                            top: 8.0, left: 5.0, right: 5.0, bottom: 8.0),
+                        child: Text('First Time Login',
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 18)),
+                      ),
+                      style: OutlinedButton.styleFrom(
+                        side: BorderSide(width: 1),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30)),
+                      )),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 34.0),
+                  child: OutlinedButton(
+                      onPressed: () async {
+                        accountLogin();
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                            top: 8.0, left: 45.0, right: 45.0, bottom: 8.0),
+                        child: Text(
+                          'Login',
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        ),
+                      ),
+                      style: OutlinedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30)),
+                        backgroundColor: Colors.black,
+                      )),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  //? Login
+  void accountLogin() async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: emailCtrl.text, password: passwordCtrl.text);
+      Navigator.of(context).pushReplacementNamed('/home');
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        loginError(context, "invalidUser");
+      } else if (e.code == 'wrong-password') {
+        loginError(context, "invalidPassword");
+      } else if (e.code == "invalid-email") {
+        loginError(context, "invalidEmail");
+      }
+    }
+  }
+
+  //? AlertDialog notification when login error is triggered
+  void loginError(BuildContext context, String errorType) {
+    String errorMsg = '';
+
+    //^ Determines error type and the appropriate message to return
+    switch (errorType) {
+      case "invalidUser":
+        errorMsg = "No user with this email has been found";
+        break;
+      case "invalidPassword":
+        errorMsg = "Sorry, your password was incorrect";
+        break;
+      case "invalidEmail":
+        errorMsg = "Please ensure that your email has been entered correctly";
+        break;
+    }
+
+    BotToast.showText(text: errorMsg);
+  }
+
+  //? Disposes of the widget once login is completed
+  @override
+  void dispose() {
+    passwordCtrl.dispose();
+    emailCtrl.dispose();
+    super.dispose();
+  }
+}
