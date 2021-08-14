46bffe9b5bca4ca94812ca8b27837fb9efabad62
WAI XIONG CHEE <wai.xiong@setel.my>
2021-08-14 16:50:32 +0800

    calendar UI and daily progress board UI

---
+++ lib/Custom_Widget/hello_calendar.dart
@@ -0,0 +1,236 @@
+import 'package:flutter/material.dart';
+
+import '../utils.dart';
+
+class HelloCalendar extends StatelessWidget {
+  final int year;
+  final int month;
+  // config data
+
+  /// the day start TB treatment
+  final DateTime from;
+  /// the day complete TB treatment
+  final DateTime to;
+  /// the day ur supply can tahan
+  final DateTime until;
+  /// dates whether video is taken, true is done
+  final Map<DateTime, bool> dates;
+
+  HelloCalendar({
+    required this.year,
+    required this.month,
+    required this.from,
+    required this.to,
+    required this.until,
+    this.dates = const {},
+  }): assert(month <= DateTime.december && month >= DateTime.january);
+
+  String get _monthToString {
+    switch(month) {
+      case DateTime.january:
+        return 'january';
+      case DateTime.february:
+        return 'february';
+      case DateTime.march:
+        return 'march';
+      case DateTime.april:
+        return 'april';
+      case DateTime.may:
+        return 'may';
+      case DateTime.june:
+        return 'june';
+      case DateTime.july:
+        return 'july';
+      case DateTime.august:
+        return 'august';
+      case DateTime.september:
+        return 'september';
+      case DateTime.october:
+        return 'october';
+      case DateTime.november:
+        return 'november';
+      case DateTime.december:
+        return 'december';
+    }
+    throw Exception('Month invalid');
+  }
+
+  Widget _weekHeader() {
+    return SizedBox(
+      height: 30,
+      child: Row(
+        children: [
+          Expanded(
+            child: Text(
+              'SUN',
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Expanded(
+            child: Text(
+              'MON',
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Expanded(
+            child: Text(
+              'TUE',
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Expanded(
+            child: Text(
+              'WED',
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Expanded(
+            child: Text(
+              'THU',
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Expanded(
+            child: Text(
+              'FRI',
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Expanded(
+            child: Text(
+              'SAT',
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Color? _computeColor(DateTime date) {
+    var b = dates[date];
+    if (dates[date]?? false) {
+      return HelloGreen;
+    }
+    // today or after that
+    if (date.isAfter(DateTime.now().subtract(const Duration(days: 1)))) {
+      if (date.isBefore(until)) {
+        // still have supply
+        return HelloYellow;
+      } else if (date.isBefore(to)) {
+        // need resupply
+        return HelloGrey;
+      }
+    } else if (date.isAfter(from)) {
+      // already pass but didnt take video
+      return HelloRed;
+    }
+    return null;
+  }
+
+  Widget _dayWidget(
+    BuildContext context, 
+    DateTime date,
+  ) {
+    return DayBox(
+      size: MediaQuery.of(context).size.width / 7,
+      day: date.day,
+      color: _computeColor(date),
+      border: date.day == DateTime.now().day,
+    );
+  }
+
+  _weekBuilder(BuildContext context, List<DateTime> week) {
+    return Row(
+      mainAxisAlignment: week[0].day == 1
+          ? MainAxisAlignment.end
+          : MainAxisAlignment.start,
+      children: week.map((day) {
+        return _dayWidget(context, day);
+      }).toList(),
+    );
+  }
+
+  Widget _calendarBuilder(
+    BuildContext context
+  ) {
+    List<List<DateTime>> weeks = [];
+    var dayDate = DateTime(year, month);
+    while (dayDate.month == month) {
+      weeks.add([]);
+      do {
+        weeks[weeks.length - 1].add(dayDate);
+        dayDate = dayDate.add(const Duration(days: 1));
+      } while(dayDate.weekday != DateTime.sunday && dayDate.month == month);
+    }
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: weeks.map<Widget>((week) {
+        return _weekBuilder(context, week);
+      }).toList(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        // month year
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 12),
+          child: Text(
+            '${_monthToString.toUpperCase()} $year',
+            style: TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        SizedBox(height: 12),
+        // week header
+        _weekHeader(),
+        // build calendar
+        _calendarBuilder(context),
+      ],
+    );
+  }
+}
+
+class DayBox extends StatelessWidget {
+  final double size;
+  final int day;
+  final Color? color;
+  final bool border;
+
+  DayBox({
+    this.size = 60,
+    this.day = 1,
+    this.color,
+    this.border = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: size, width: size,
+      padding: EdgeInsets.all(3),
+      child: Container(
+        decoration: color != null
+          ? BoxDecoration(
+              color: color,
+              borderRadius: BorderRadius.all(Radius.circular(size/2-3)),
+              border: border
+                ? Border.all(
+                  color: HelloBlue,
+                ) : null,
+            )
+          : null,
+        child: Center(
+          child: Text('$day'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Screens/daily_progress_board.dart
@@ -0,0 +1,77 @@
+import 'package:tb_e_health/Custom_Widget/hello_calendar.dart';
+import 'package:flutter/material.dart';
+
+import 'package:tb_e_health/utils.dart';
+
+class DailyProgressBoardScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var today = DateTime.now().getToday();
+    // TODO: get state
+    var dates = {
+      DateTime(2021, 8, 5): true,
+      DateTime(2021, 8, 6): true,
+      DateTime(2021, 8, 7): true,
+      DateTime(2021, 8, 8): true,
+      DateTime(2021, 8, 9): true,
+      DateTime(2021, 8, 10): true,
+      DateTime(2021, 8, 12): true,
+      DateTime(2021, 8, 13): true,
+    };
+    return Scaffold(
+      // TODO: primary color
+      appBar: AppBar(
+        elevation: 0,
+      ),
+      body: Column(
+        children: [
+          HelloCalendar(
+            year: today.year,
+            month: today.month,
+            // TODO: get from state
+            from: DateTime(2021, 8, 5),
+            to: DateTime(2021, 8, 30),
+            until: DateTime(2021, 8, 26),
+            dates: dates,
+          ),
+          Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(100.0),
+            ),
+            child: SizedBox(
+              height: 200,
+              width: 200,
+              child: Center(
+                child: Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Text(
+                    dates[today]?? false 
+                        // today taken medication
+                        ? 'YOU HAVE COMPLETED YOUR MEDICATION TODAY!'
+                        : 'HAVE YOU TAKE YOUR MEDICATION?',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: Text(
+                'Keep up the good work!',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
