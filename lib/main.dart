d150f96e353f5efd1fcd6b5e9e114f317f3849e9
lydia0423 <55584148+lydia0423@users.noreply.github.com>
2021-08-16 20:11:09 +0800

    Add device preview and forget password function
    -device preview: can be used to check the UI on different device
    - forget password: user can reset their password by entering the email and a reset password email will be sent out to their email

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,27 @@
 // import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:device_preview/device_preview.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:tb_e_health/Screens/home_screen.dart';
 import 'package:tb_e_health/Screens/login.dart';
 import 'package:tb_e_health/Screens/navigations.dart';
 
 main() async {
+  bool kReleaseMode = false;
   //~ initialises firebase instances for authentication and Cloud FireStore
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   FirebaseAuth.instance;
   //~ Redirects users
-  String myRoute = "/";
+  String myRoute = await checkLoggedIn();
+  //String myRoute = "/";
   //~ Run application
-  runApp(MyApp(myRoute));
+  runApp(DevicePreview(
+    enabled: kReleaseMode,
+    builder: (context) => MyApp(myRoute),
+  ));
 }
 
 class MyApp extends StatefulWidget {
@@ -30,6 +36,8 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+        locale: DevicePreview.locale(context),
+        builder: DevicePreview.appBuilder,
         debugShowCheckedModeBanner: false,
         title: "TB e-health",
         theme: ThemeData.light(),
@@ -40,4 +48,24 @@ class _MyAppState extends State<MyApp> {
           "/home": (context) => Navigations(),
         });
   }
+}
+
+Future<String> checkLoggedIn() async {
+  String myRoute = "/home";
+  try {
+    User? currentUserId = FirebaseAuth.instance.currentUser;
+    if (currentUserId == null) {
+      myRoute = "/";
+    } else {
+      await FirebaseFirestore.instance
+          .collection("User")
+          .doc(currentUserId.uid)
+          .get();
+
+      myRoute = "/home";
+    }
+  } catch (e) {
+    print("$e: User is not logged in");
+  }
+  return myRoute;
 }
