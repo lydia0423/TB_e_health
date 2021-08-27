b8d2706f2a058419f3bc06c5d8ebedc94c8f70ac
GBNETUK\seekiat.ng <seekiat.ng@gbgplc.com>
2021-08-28 00:04:13 +0800

    - put the DevicePreview back to main.

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,22 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:device_preview/device_preview.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 import 'package:tb_e_health/models/anonymous_user.dart';
-import 'package:tb_e_health/screens/daily_progress_board.dart';
-import 'package:tb_e_health/screens/login.dart';
-import 'package:tb_e_health/screens/teleconsultation/session_screen.dart';
 import 'package:tb_e_health/screens/wrapper.dart';
 import 'package:tb_e_health/services/auth_service.dart';
-
-import 'screens/teleconsultation/scheduler_screen.dart';
-import 'package:tb_e_health/screens/navigations.dart';
-import 'package:tb_e_health/theme.dart';
 
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+
+  bool kReleaseMode = false;
+
+  runApp(DevicePreview(
+    enabled: kReleaseMode,
+    builder: (context) => MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
