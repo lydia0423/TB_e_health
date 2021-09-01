7f266f017e26da0282503f267d57ca2d96294a71
lydia0423 <55584148+lydia0423@users.noreply.github.com>
2021-09-01 17:55:08 +0800

    Update sign_in.dart

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -255,9 +255,6 @@ class _SignInState extends State<SignIn> {
                                 dynamic result =
                                     await _auth.signInWithUserIdAndPassword(
                                         emailCtrl.text, passwordCtrl.text);
-                                setState(() {
-                                  loading = false;
-                                });
                                 if (result is String) {
                                   print('SignIn: error!!! ' + result);
                                   loginError(context, result);
